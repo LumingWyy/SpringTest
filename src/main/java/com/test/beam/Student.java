47f182f77d890c77bb009c48ed3539eb162a1565
wlm <599290100@qq.com>
2022-01-19 21:01:14 +0900

    testday2

--- src/main/java/com/test/beam/Student.java
+++ src/main/java/com/test/beam/Student.java
@@ -1,10 +1,46 @@
 package com.test.beam;
 
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Map;
+
+@ToString
 public class Student {
     String name;
     int age;
-
+    Card card;
+    List<String> list;
+    Map<String ,String> map;
+    
+    
     public  Student() {
         System.out.println("student constructor");
+    }
+
+
+    public  Student(String name) {
+        System.out.println("student constructor2");
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
     }
 }
